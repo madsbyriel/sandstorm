e388e1d21a0bec96b1a96277f38b5dc4e770b803
Mads Byriel <madshbyriel@gmail.com>
2026-04-18 13:34:21 +0200

    All shit

---
+++ lua/reactive/presets/sandstorm-frappe-cursor.lua
@@ -0,0 +1,4 @@
+local palette = require("sandstorm.palettes").get_palette "frappe"
+local presets = require "sandstorm.utils.reactive"
+
+return presets.cursor("sandstorm-frappe-cursor", palette)
---
+++ lua/reactive/presets/sandstorm-macchiato-cursor.lua
@@ -0,0 +1,4 @@
+local palette = require("sandstorm.palettes").get_palette "macchiato"
+local presets = require "sandstorm.utils.reactive"
+
+return presets.cursor("sandstorm-macchiato-cursor", palette)
